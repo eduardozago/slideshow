f5cd99c6ca133b2e82c41b3fd789866b4728032c
eduardozago <zagoe22@gmail.com>
2022-11-08 13:02:41 -0300

    slideshow working

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+let numSlides = document.querySelectorAll('.slide').length;
+
+let currentSlide = 0;
+
+document.querySelector('.slides').style.width = `calc(100vw * ${numSlides})`;
+document.querySelector('.slider-buttons').style.height = `${document.querySelector('.slider').clientHeight}px`;
+
+function goPrevious() {
+    currentSlide--;
+
+    if(currentSlide < 0) {
+        currentSlide = numSlides - 1;
+    }
+
+    updateMargin();
+}
+
+function goNext() {
+    currentSlide++;
+
+    if(currentSlide > (numSlides - 1)) {
+        currentSlide = 0;
+    }
+
+    updateMargin();
+}
+
+function updateMargin() {
+    let newMargin = (currentSlide * document.body.clientWidth);
+
+    document.querySelector('.slides').style.marginLeft = `-${newMargin}px`;
+}
+
+setInterval(goNext, 4000);
